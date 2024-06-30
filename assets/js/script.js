2709ad7c0eebca6f1d8cf6667f890929e2898314
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-06-29 23:26:10 -0500

    generateTaskId and createTaskCard functions filled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,63 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const addTaskBtn = $('add-task-btn');
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('div')
+        .addClass('card project-card draggable my-3')
+        .attr('data-project-id', generateTaskId());
+
+    const taskHeader = $('<div>')
+            .addClass('card-header h4')
+            .text(task.name);
+    
+    const taskBody = $('<div>').addClass('card-body');
+   
+    const taskDescription = $('<p>')
+        .addClass('card-text')
+        .text(task.descrip);
+    
+    const taskDueDate = $('<p>')
+        .addClass('card-text')
+        .text(task.dueDate);
+    
+    const taskDeleteBtn = $('<button>')
+        .addClass('btn btn-danger delete')
+        .text('Delete').attr('data-project-id', project.id);
+
+
+    if (task.dueDate && task.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    
+        // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+        if (now.isSame(taskDueDate, 'day')) {
+            taskCard.addClass('bg-warning text-white');
+        } else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass('bg-danger text-white');
+            taskDeleteBtn.addClass('border-light');
+        }
+    }
+
+    taskBody.append(taskDescription, taskDueDate, taskDeleteBtn);
+    taskCard.append(taskHeader, taskBody);
+    
+    return taskCard;
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
+function renderTaskList(tasks) { 
+    
+   
 }
 
 // Todo: create a function to handle adding a new task
